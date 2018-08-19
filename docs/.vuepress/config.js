ff1ec6fae07d2a7fd0010c4246e2bf221d2c274d
huaxiaolong <361001678@qq.com>
2018-08-19 10:50:27 +0800

    first deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '风之谷',
+    description: '技术博客',
+    themeConfig: {
+        nav: [
+            { text: '笔记', link: '/note/' },
+            { text: 'leetcode', link: '/leetcode/' },
+            { text: 'github', link: 'http://github.com/huaxiaolong' }
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated'
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /hero.png
+actionText: 欢迎回来
+actionLink: /note/
+footer: by xiaolong
+---
